a866ed642047817bd9a40bf3e5cc4b873d0a69dd
VolviX <20700808+VolviX@users.noreply.github.com>
2025-01-12 22:00:23 +0300

    dunno what im doing, switching computer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameboard = {
-    firstRow: ['_', ' | ', ' _ ', ' | ', '_'],
-    secondRow: ['_', ' | ', ' _ ', ' | ', '_'],
-    thirdRow: ['_', ' | ', ' _ ', ' | ', '_'],
+    firstRow: ['1', ' | ', '2', ' | ', '3'],
+    secondRow: ['4', ' | ', '5', ' | ', '6'],
+    thirdRow: ['7', ' | ', '8', ' | ', '9'],
     display() {
         console.log(
             this.firstRow.join('') + '\n' +
@@ -22,9 +22,60 @@ const player = {
 };
 
 const game = {
-    // game logic
+    playerTurn: true, // true for X and false for O
+    turn() {
+        if (this.playerTurn) {
+            console.log('Player One\'s turn');
+        } else {
+            console.log('Player Two\'s turn');
+        }
+
+        while (true) {
+            const userInput = prompt('Position (1-9): ');
+
+            if (userInput > 0 && userInput < 10) {
+                if (userInput > 0 && userInput < 4) {
+                    // first row
+                    const index = gameboard.firstRow.indexOf(userInput);
+                    
+                    if (this.playerTurn) {
+                        gameboard.firstRow[index] = playerOne.marker;
+                    } else {
+                        gameboard.firstRow[index] = playerTwo.marker;
+                    }
+                } else if (userInput > 3 && userInput < 7) {
+                    // second row
+                    const index = gameboard.secondRow.indexOf(userInput);
+                    
+                    if (this.playerTurn) {
+                        gameboard.secondRow[index] = playerOne.marker;
+                    } else {
+                        gameboard.secondRow[index] = playerTwo.marker;
+                    }
+                } else {
+                    // third row
+                    const index = gameboard.thirdRow.indexOf(userInput);
+                    
+                    if (this.playerTurn) {
+                        gameboard.thirdRow[index] = playerOne.marker;
+                    } else {
+                        gameboard.thirdRow[index] = playerTwo.marker;
+                    }
+                }
+                this.playerTurn = !this.playerTurn;
+                gameboard.display();
+                break
+            } else {
+                console.log('Invalid position, try again.')
+            }
+        }
+    },
+    didSomeoneWin() {
+        // checks positions for a winner or draw
+    },
 };
 
 gameboard.display();
 const playerOne = player.create({name: "John", marker: "X", score: 0});
 const playerTwo = player.create({name: "Jane", marker: "O", score: 0});
+game.turn();
